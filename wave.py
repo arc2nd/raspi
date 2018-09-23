c7c3befb339919bc45e176f3419ee850d74cd516
James Parks <james_parks@hotmail.com>
2018-09-23 09:27:19 -0700

    made wave wider

--- wave.py
+++ wave.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import Servo
 
 myS = Servo.Servo()
 
 for i in range(0, 4):
-    myS.sweep(60, 110, 0.01)
-    myS.sweep(110, 60, 0.01)
+    myS.sweep(60, 120, 0.01)
+    myS.sweep(120, 60, 0.01)
 
 
