e6442fcd618b3d929920b790b54b978bab5a4c7e
James Parks <james_parks@hotmail.com>
2018-10-27 09:02:30 -0700

    added arg to wave.py

--- wave.py
+++ wave.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import sys
 
 import Servo
 
+times = sys.argv[1]
 myS = Servo.Servo()
 
-for i in range(0, 4):
+for i in range(0, int(times)):
     myS.sweep(60, 120, 0.01)
     myS.sweep(120, 60, 0.01)
 
